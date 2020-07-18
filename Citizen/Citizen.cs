bfe4e7de800cb89a22b487dace949b39a5db3ef4
tomerbibi <66681821+tomerbibi@users.noreply.github.com>
2020-07-18 21:39:27 +0300

    Add files via upload

---
+++ Citizen/Citizen.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Citizen
+{
+    class Citizen
+    {
+        public string _name;
+        public Citizen[] _children;
+        public readonly int _id;
+        public readonly int _fatherId;
+        public static int _numberOfCurrentCitizens;
+        public const int _maximumNumberOfCitizens = 10000000;
+        public Citizen (string name , int fatherId)
+        {
+            _name = name;
+            _fatherId = fatherId;
+        }
+        public static void PrintNumberOfCitizens()
+        {
+            Console.WriteLine(_numberOfCurrentCitizens);
+        }
+        public static bool ReachedHalfOfMaximumSize()
+        {
+            if (_numberOfCurrentCitizens >= _maximumNumberOfCitizens / 2)
+            {
+                return true;
+            }
+            return false;
+        }
+        public void PrintId ()
+        {
+            Console.WriteLine(_id);
+        }
+        public void PringGapBetweenMyIdAndFather()
+        {
+            Console.WriteLine(_fatherId - _id);
+        }
+        public void SetChildren(Citizen[] c)
+        {
+         for (int i = 0; i < c.Length; i++)
+            {
+                c[i] = _children[i];
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"name: {_name}, ID: {_id}, father ID: {_fatherId}, amount of children: {_children.Length}";
+        }
+
+    }
+}
---
+++ Citizen/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Citizen
+{
+    class Program
+    {
+        public static bool HadChildren(Citizen c)
+        {
+            if (c._children.Length == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+        public static bool CheckValidity(Citizen c)
+        {
+            if (HadChildren(c))
+            {
+                for (int i = 0; i < c._children.Length; i++)
+                {
+                    if (c._children[i]._fatherId != c._id)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Citizen bob = new Citizen("bob", 2738);
+            Citizen lacy = new Citizen("lacy", 2908);
+            Citizen jina = new Citizen("jina", 2990);
+            Citizen dan = new Citizen("dan", 2977);
+            Citizen[] c = new Citizen[3] {lacy,jina,dan};
+            bob._children = c;
+
+        }
+    }
+}
